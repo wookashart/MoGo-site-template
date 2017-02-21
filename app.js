0cb2606f4164060f2a142f69b3c8aff593807ab6
Łukasz Krawczyk <ukasz Krawczyk>
2017-02-21 12:37:29 +0100

    update - navigation menu

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
+// navigation
 
+$('#menu-about').on('click', function(){
+    $("html, body").animate({ scrollTop: $('#storyAboutUs').offset().top - 170}, 1000);
+});
+
+$('#menu-service').on('click', function(){
+    $("html, body").animate({ scrollTop: $('#amazingServices').offset().top - 190}, 1000);
+});
+
+$('#menu-work').on('click', function(){
+    $("html, body").animate({ scrollTop: $('#someWork').offset().top - 190}, 1000);
+});
+
+$('#menu-blog').on('click', function(){
+    $("html, body").animate({ scrollTop: $('#blog').offset().top - 190}, 1000);
+});
+
+$('#menu-contact').on('click', function(){
+    $("html, body").animate({ scrollTop: $('#maybe-contact').offset().top - 190}, 1000);
+});
+
+$('#learnMore').on('click', function(){
+    $("html, body").animate({ scrollTop: $('#storyAboutUs').offset().top - 170}, 1000);
+});
 
 //accordion
 
