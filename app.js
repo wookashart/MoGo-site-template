979d9874e1cd3ae77ad63c0fd73123d88f9d4178
Łukasz Krawczyk <ukasz Krawczyk>
2017-02-21 21:50:03 +0100

    update - mobile first

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
-// navigation
+//hamburger
+
+$('#hamburger').on('click', function(){
+    $(this).toggleClass('open');
+    $('nav.nav').toggleClass('down');
+})
+
+//navigation
 
 $('#menu-about').on('click', function(){
     $("html, body").animate({ scrollTop: $('#storyAboutUs').offset().top - 170}, 1000);
+    $('nav.nav').removeClass('down');
+    $('#hamburger').removeClass('open');
 });
 
 $('#menu-service').on('click', function(){
     $("html, body").animate({ scrollTop: $('#amazingServices').offset().top - 190}, 1000);
+    $('nav.nav').removeClass('down');
+    $('#hamburger').removeClass('open');
 });
 
 $('#menu-work').on('click', function(){
     $("html, body").animate({ scrollTop: $('#someWork').offset().top - 190}, 1000);
+    $('nav.nav').removeClass('down');
+    $('#hamburger').removeClass('open');
 });
 
 $('#menu-blog').on('click', function(){
     $("html, body").animate({ scrollTop: $('#blog').offset().top - 190}, 1000);
+    $('nav.nav').removeClass('down');
+    $('#hamburger').removeClass('open');
 });
 
 $('#menu-contact').on('click', function(){
     $("html, body").animate({ scrollTop: $('#maybe-contact').offset().top - 190}, 1000);
+    $('nav.nav').removeClass('down');
+    $('#hamburger').removeClass('open');
 });
 
 $('#learnMore').on('click', function(){
     $("html, body").animate({ scrollTop: $('#storyAboutUs').offset().top - 170}, 1000);
+    $('nav.nav').removeClass('down');
+    $('#hamburger').removeClass('open');
+});
+
+$('#go-home').on('click', function(){
+    $("html, body").animate({ scrollTop: 0}, 1000);
+    $('nav.nav').removeClass('down');
+    $('#hamburger').removeClass('open');
 });
 
 //accordion
