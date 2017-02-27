22d7f4df55d4bdb423e6ed23e74c72f4e5a2a429
Łukasz Krawczyk <ukasz Krawczyk>
2017-02-27 18:55:07 +0100

    update - hover

--- app.js
+++ app.js
@@ -7,47 +7,12 @@ $('#hamburger').on('click', function(){
 
 //navigation
 
-$('#menu-about').on('click', function(){
-    $("html, body").animate({ scrollTop: $('#storyAboutUs').offset().top - 170}, 1000);
+$('.nav-button').on('click', function(){
+    var target = $(this).data('target');
+    $("html, body").animate({ scrollTop: $(target).offset().top -160}, 1000);
     $('nav.nav').removeClass('down');
     $('#hamburger').removeClass('open');
-});
-
-$('#menu-service').on('click', function(){
-    $("html, body").animate({ scrollTop: $('#amazingServices').offset().top - 190}, 1000);
-    $('nav.nav').removeClass('down');
-    $('#hamburger').removeClass('open');
-});
-
-$('#menu-work').on('click', function(){
-    $("html, body").animate({ scrollTop: $('#someWork').offset().top - 190}, 1000);
-    $('nav.nav').removeClass('down');
-    $('#hamburger').removeClass('open');
-});
-
-$('#menu-blog').on('click', function(){
-    $("html, body").animate({ scrollTop: $('#blog').offset().top - 190}, 1000);
-    $('nav.nav').removeClass('down');
-    $('#hamburger').removeClass('open');
-});
-
-$('#menu-contact').on('click', function(){
-    $("html, body").animate({ scrollTop: $('footer').offset().top}, 1000);
-    $('nav.nav').removeClass('down');
-    $('#hamburger').removeClass('open');
-});
-
-$('#learnMore').on('click', function(){
-    $("html, body").animate({ scrollTop: $('#storyAboutUs').offset().top - 170}, 1000);
-    $('nav.nav').removeClass('down');
-    $('#hamburger').removeClass('open');
-});
-
-$('#go-home').on('click', function(){
-    $("html, body").animate({ scrollTop: 0}, 1000);
-    $('nav.nav').removeClass('down');
-    $('#hamburger').removeClass('open');
-});
+})
 
 //accordion
 
@@ -55,3 +20,11 @@ $('button.accordion-button').on('click', function(e){
     $('.accordion-panel').removeClass('down');
     $(this).next().toggleClass('down');
 })   
+
+$(window).on('scroll', function() {
+    if ($('body').scrollTop() >= 150) {
+        $('div.navigation').addClass('scrolled');
+    } else {
+        $('div.navigation').removeClass('scrolled');
+    }
+})
